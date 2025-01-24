66e7f2bfe6c051c8d5b40c040d81bef4d28d184b
Rodolfo Gonzalez <rodolfo.gonzalez+github@gmail.com>
2025-01-24 01:43:50 -0600

    Migrated to @postalsys/gettext to workaround CVE-2024-21528

--- src/index.svelte
+++ src/index.svelte
@@ -32,7 +32,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import { derived } from 'svelte/store';
 import { parsedTranslations, lang } from './stores';
-import Gettext from 'node-gettext';
+import Gettext from '@postalsys/gettext';
 
 const gt = new Gettext();
 
